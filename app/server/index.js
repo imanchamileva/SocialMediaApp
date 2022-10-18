7dcb51bb3879975105300897a7ec64eb0a10d502
Iman Chamileva <iman.moussaevna@gmail.com>
2022-10-18 16:39:00 +0200

    controllers and models in process

--- app/server/index.js
+++ app/server/index.js
@@ -4,29 +4,42 @@ const PORT = process.env.PORT || 5000
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+import postRoutes from './routes/posts.js';
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/', (req, res) => {
-    res.send('server started on port 5000')
-})
+app.use('/posts', postRoutes)
 
 
 const uri = "mongodb+srv://adminUser:Lalala7@cluster0.apbujcz.mongodb.net/?retryWrites=true&w=majority";
 
-async function connect() {
+// async function connect() {
 
-    try {
-        await mongoose.connect(uri);
-        console.log("connected to MongoDB");
-    } catch (error) {
-        console.error(error);
-    }
-}
+//     try {
+//         await mongoose.connect(uri);
+//         console.log("connected to MongoDB");
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
-connect();
+// connect();
+
+
+mongoose.connect(uri, {
+  useNewUrlParser: "true",
+})
+mongoose.connection.on("error", err => {
+    console.log("err", err)
+  })
+  mongoose.connection.on("connected", (err, res) => {
+    console.log("mongoose is connected")
+  })
+
+
+
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
---
+++ app/server/models/postMessage.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+//each message will have these things
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags : [String],
+    selectedFile : String,
+    likeCount :{
+        type: Number,
+        default: 0,
+    },
+    createdAt : {
+        type: Date,
+        default: new Date(),
+    }
+})
+
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
