19629ea2cbdab36267389fb48f8b3332c60edc5f
Iman Chamileva <iman.moussaevna@gmail.com>
2022-11-04 13:17:11 +0100

    blur

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="font-inter text-red-800 text-3xl">
-     <h1>hello</h1>
-    </div>
+    <section className="bg-gray-100 h-screen w-screen">
+      <div className="w-72 h-56 rounded-full bg-blue-200 blur-2xl absolute my-80"></div>
+      <div className="w-80 h-72 rounded-full bg-blue-200 blur-2xl absolute inset-y-0 right-0"> </div>
+    </section>
 
   );
 }
