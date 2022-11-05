1f37dc2d9e524c5d3afbfab72eeb9266d3b8247b
Iman Chamileva <iman.moussaevna@gmail.com>
2022-11-06 00:04:08 +0100

    profile img added

--- app/client/src/components/ProfileCard.jsx
+++ app/client/src/components/ProfileCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import Cover from '../images/cover.jpg'
+import ProfileImg from '../images/profileImg.jpg'
 
 const ProfileCard = () => {
-  return (
-    <div className="pl-4 pt-4">
-        <div className="h-96 w-80 bg-red-500 rounded-lg">
+    return (
+        <div className="pl-4 pt-4">
+            <div className="h-80 w-64 bg-red-500 rounded-lg">
+                <img src={Cover} alt="profilecard_cover" className="rounded-t-lg relative" />
+                <div className="flex justify-center">
+                    <img src={ProfileImg} alt="profilecard_rofile_image" className="rounded-full w-24 h-24 absolute" />
+                </div>
 
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProfileCard
