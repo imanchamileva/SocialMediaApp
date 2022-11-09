a6b518d23cce765d1fbfb785955abaee3ed18f4a
Iman Chamileva <iman.moussaevna@gmail.com>
2022-11-09 15:19:34 +0100

    delete user

--- app/server/controllers/UserController.js
+++ app/server/controllers/UserController.js
@@ -1,5 +1,5 @@
 import UserModel from '../models/userModel.js';
-
+import bcrypt from 'bcrypt';
 
 // get a user from the
 
@@ -11,10 +11,13 @@ export const getUser = async (req, res) => {
 
         if (user) {
 
-            const {password, ...otherDetails} = user._doc
-            res.status(200).json(otherDetails);
+            const { password, ...otherDetails } = user._doc
+            res.status(200).json(otherDetails)
         }
 
+        else {
+            res.status(404).json("no such user exists")
+        }
     } catch (error) {
 
         res.status(500).json(error)
@@ -22,3 +25,61 @@ export const getUser = async (req, res) => {
 
 
 }
+
+// update a user 
+
+export const updateUser = async (req, res) => {
+    const id = req.params.id
+    const { currentUserId, currentUserAdminStatus, password } = req.body
+
+    if (id === currentUserId || currentUserAdminStatus) {
+
+        try {
+
+            if (password) {
+
+                const salt = await bcrypt.genSalt(10)
+                //will convert the body of request
+                req.body.password = await bcrypt.hash(password, salt)
+            }
+
+            const user = await UserModel.
+                //id= search the id to update in db, req.body= info to update in response, new:true= in response I want to get the updated user not the old one
+                findByIdAndUpdate(id, req.body, { new: true })
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
+    else {
+        res.status(403).json("Access denied ! You can only update your own profile")
+    }
+}
+
+// delete user 
+
+export const deleteUser = async (req, res) => {
+
+    const id = req.params.id
+
+    const { currentUserId, currentUserAdminStatus } = req.body
+
+    if (currentUserId === id || currentUserAdminStatus) {
+
+        try {
+
+            await UserModel.findByIdAndDelete(id)
+            res.status(200).json("User deleted successfully")
+
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    else {
+        res.status(403).json("Access denied ! You can only delete your own profile")
+    }
+}
+
+// follow a user
+
--- app/server/routes/UserRoute.js
+++ app/server/routes/UserRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getUser } from "../controllers/UserController.js";
+import { getUser, updateUser, deleteUser } from "../controllers/UserController.js";
 
 const router = express.Router();
 
 router.get('/:id', getUser)
-
-
+//put : to update a user
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 
 export default router;
