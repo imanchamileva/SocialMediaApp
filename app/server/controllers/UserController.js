6357e8e31effdd009191e281ff7e3d2fa88256cd
Iman Chamileva <iman.moussaevna@gmail.com>
2022-11-09 20:28:29 +0100

    follow and unfollow user

--- app/server/controllers/UserController.js
+++ app/server/controllers/UserController.js
@@ -83,3 +83,63 @@ export const deleteUser = async (req, res) => {
 
 // follow a user
 
+export const followUser = async (req, res) => {
+    const id = req.params.id
+
+    const { currentUserId } = req.body
+
+    if (currentUserId === id) {
+        res.status(403).json("Action forbidden !")
+    }
+    else {
+        try {
+
+            const followUser = await UserModel.findById(id)
+            const followingUser = await UserModel.findById(currentUserId)
+
+            //if already following him
+            if (!followUser.followers.includes(currentUserId)) {
+
+                await followUser.updateOne({ $push: { followers: currentUserId } })
+                await followingUser.updateOne({ $push: { following: id } })
+                res.status(200).json("User followed !")
+            }
+            else {
+                res.status(403).json("User is already followed by you")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
+
+//unfollow user
+export const UnFollowUser = async (req, res) => {
+    const id = req.params.id
+
+    const { currentUserId } = req.body
+
+    if (currentUserId === id) {
+        res.status(403).json("Action forbidden !")
+    }
+    else {
+        try {
+
+            const followUser = await UserModel.findById(id)
+            const followingUser = await UserModel.findById(currentUserId)
+
+            //if already following him
+            if (followUser.followers.includes(currentUserId)) {
+
+                await followUser.updateOne({ $pull: { followers: currentUserId } })
+                await followingUser.updateOne({ $pull: { following: id } })
+                res.status(200).json("User unfollowed !")
+            }
+            else {
+                res.status(403).json("User is not by followed by you")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
--- app/server/routes/UserRoute.js
+++ app/server/routes/UserRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, updateUser, deleteUser } from "../controllers/UserController.js";
+import { getUser, updateUser, deleteUser, followUser, UnFollowUser } from "../controllers/UserController.js";
 
 const router = express.Router();
 
@@ -7,6 +7,9 @@ router.get('/:id', getUser)
 //put : to update a user
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', UnFollowUser)
+
 
 
 export default router;
